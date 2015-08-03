3d9b78a0bfbc0311cdc7389c33a6fd3e9aa6323f
LunaLMM <ljb8901@163.com>
2015-08-03 22:58:34 +0200

    filter test2

---
+++ JBLMM/Element/filters_test.cpp
@@ -0,0 +1,12 @@
+#include "filters_test.h"
+#include <JBLMM/Element/CouponLeg.h>
+#include <JBLMM/Element/Coupon.h>
+
+filters_test::filters_test(void)
+{
+}
+
+
+filters_test::~filters_test(void)
+{
+}
---
+++ JBLMM/Element/filters_test.h
@@ -0,0 +1,8 @@
+#pragma once
+class filters_test
+{
+public:
+	filters_test(void);
+	~filters_test(void);
+};
+
